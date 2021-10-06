4e96175849af619d5daba737cb196bfc5191e13d
JAYDIPSINH27 <jaydipadhiyar27@gmail.com>
2021-10-06 14:46:11 +0530

    carousel update

--- client/src/components/Homepage/Carousel.jsx
+++ client/src/components/Homepage/Carousel.jsx
@@ -6,16 +6,28 @@ import React from 'react'
 import AwesomeSlider from 'react-awesome-slider';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
+import styled from "styled-components"
 import Home1 from '../../assets/Home1.jpg'
 import Home2 from '../../assets/Home2.jpg'
 import Home3 from '../../assets/Home3.jpg'
 
+
+const Container = styled.div`
+    padding:2rem 0rem;
+    width:100%;
+    height:90vh;
+    display:flex;
+    position:relative;
+    /* overflow:hidden; */
+`
+
 const AutoplaySlider = withAutoplay(AwesomeSlider);
+
 
 const Carousel = () => {
 
     return (
-        <div>
+        <Container>
             <AutoplaySlider
                 play={true}
                 cancelOnInteraction={true} // should stop playing on user interaction
@@ -27,7 +39,7 @@ const Carousel = () => {
                 <div data-src={Home2} />
                 <div data-src={Home3} />
             </AutoplaySlider>
-                    </div>
+        </Container>
     )
 }
 
